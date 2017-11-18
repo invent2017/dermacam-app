a15af4ef3100ab880d5250628d802689472d9ddb
pyraxo <pyraxo@tfwno.gf>
2017-11-18 14:56:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# DermaCam
+
+Self-diagnosis app that can identify various skin diseases using your phone camera.
+
+Still work-in-progress, check back later.
+
+## License
+
+Refer to the [License](LICENSE) file.
---
+++ components/Navigation.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Notifications } from 'expo';
+import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { StackNavigator, TabNavigator, TabBarBottom } from 'react-navigation';
+
+import Colors from '../constants/Colors';
+
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import CameraScreen from '../screens/CameraScreen';
+
+const MainTabNavigator = TabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Camera: {
+      screen: CameraScreen,
+    }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        switch (routeName) {
+          case 'Home':
+            iconName =
+              Platform.OS === 'ios' ? `ios-home${focused ? '' : '-outline'}` : 'md-home';
+            break;
+          case 'Links':
+            iconName = Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link';
+            break;
+          case 'Settings':
+            iconName = Platform.OS === 'ios'
+            ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
+            break;
+          case 'Camera':
+            iconName = Platform.OS === 'ios'
+            ? `ios-camera${focused ? '' : '-outline'}` : 'md-camera';
+        }
+        return (
+          <Ionicons
+            name={iconName}
+            size={28}
+            style={{ marginBottom: -3 }}
+            color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+          />
+        );
+      },
+    }),
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    swipeEnabled: false,
+  }
+);
+
+export default StackNavigator(
+  {
+    Main: {
+      screen: MainTabNavigator,
+    },
+  },
+  {
+    navigationOptions: () => ({
+      headerTitleStyle: {
+        fontWeight: 'normal',
+      },
+    }),
+  }
+);
